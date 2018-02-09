3f55d83c1adb2799bcea046490fc4a07eb838f76
Shawn Thye <shawn.thye@gmail.com>
2018-02-09 20:52:01 +0800

    init

---
+++ token-authentication-sample/Security/AuthenticationOptions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace tokenauthenticationsample.Security
+{
+    public class CustomAuthOptions : AuthenticationSchemeOptions
+    {
+        
+        public const string DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        public string Scheme => DefaultAuthenticateScheme;
+        public StringValues AuthKey { get; set; }
+    }
+
+
+    public class CustomAuthHandler : AuthenticationHandler<CustomAuthOptions>
+    {
+        public CustomAuthHandler(IOptionsMonitor<CustomAuthOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+
+            string authorization = Request.Headers["Authorization"];
+
+            string token = null;
+
+            if (authorization.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+            {
+                token = authorization.Substring("Bearer ".Length).Trim();
+            }
+
+            if (string.IsNullOrEmpty(token))
+            {
+                return Task.FromResult(AuthenticateResult.NoResult()) ;
+            }
+
+            var identities = new List<ClaimsIdentity> { new ClaimsIdentity("custom auth type") };
+            var ticket = new AuthenticationTicket(new ClaimsPrincipal(identities), Options.Scheme);
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+           
+
+            return Task.FromResult(AuthenticateResult.Success());
+        }
+    }
+
+
+
+
+
+    public class TokenRequirement : IAuthorizationRequirement
+    {
+        
+    }
+
+    public class MyAuthenticationHandler : Microsoft.AspNetCore.Authorization.AuthorizationHandler<TokenRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, TokenRequirement requirement)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ token-authentication-sample/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace token_authentication_sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(o=> {
+
+
+            o.Filters.Add(new AuthorizeFilter(new AuthorizationPolicyBuilder()
+                                              .RequireAuthenticatedUser()
+                                              .Build()
+                ));
+            });
+
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
